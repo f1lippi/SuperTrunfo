1f4ca15f6648880b2079d6db3b0251ae659962ac
f1lippi <ctt.filippi@gmail.com>
2025-06-19 20:56:00 +0000

    Novo commit

---
+++ EntradaSaidaDados.c
@@ -0,0 +1,11 @@
+int main(){
+    int idade = 25;
+    float altura = 1.75;
+    char opcao = 'F';
+    char nome[20] = "filippi";
+
+    printf("A idade do %s é %d\n", nome, idade);
+    printf("A altura é: %e\n", altura);
+    printf("A opção é: %c\n, opcao");
+
+}
---
+++ HoraCodar.c
@@ -0,0 +1,19 @@
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50]
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite o seu nome: \n");
+    scanf("%s", &nome)
+
+    printf("Nome do aluno: %s = Matrícula: %d", nome, matricula);
+    printf("Idade: %d - Altura: %f", idade, altura);
+
+
+}
